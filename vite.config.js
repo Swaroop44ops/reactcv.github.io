c5a676745a97a425891cb29a834964f988a0555d
Swaroop44ops <58067776+Swaroop44ops@users.noreply.github.com>
2025-09-17 17:06:02 +0530

    changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/reactcv.github.io/", // 👈 match repo name exactly
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/reactcv.github.io/",   // 👈 keep it exactly like this
+});
