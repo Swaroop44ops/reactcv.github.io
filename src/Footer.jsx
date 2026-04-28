e050f28bba6f8a361fa9bf0f708ecb1b2e8df49c
Swaroop H G <swaroop.hg@ideainfinityit.com>
2026-04-28 17:59:46 +0530

    chnages for footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            © 2025 Swaroop H G
+            ï¿½ 2026 Swaroop H G
         </footer>
     );
 }
